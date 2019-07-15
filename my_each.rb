19d65823b2ee386aea2a1b2bc5eccb7da44b85a2
Pablo Gonzalez <pabgon0@gmail.com>
2019-07-15 18:42:12 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,12 @@
 def my_each(collection)
   i = 0
   while i < collection.length
-    yield 
+    yield collection[i]
   i += 1
   end
   collection
+end
+
+my_each(collection) do |number|
+  "number"
 end
